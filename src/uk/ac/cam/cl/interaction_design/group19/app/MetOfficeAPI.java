fd6aa47302a0a933638e083dd1dbfd80a10b8858
Roddy <rlm72@cam.ac.uk>
2018-05-10 15:25:45 +0100

    Added library for parsing JSON

--- src/uk/ac/cam/cl/interaction_design/group19/app/MetOfficeAPI.java
+++ src/uk/ac/cam/cl/interaction_design/group19/app/MetOfficeAPI.java
@@ -3,4 +3,6 @@ package uk.ac.cam.cl.interaction_design.group19.app;
 public class MetOfficeAPI {
     private static final String KEY = "6d150446-6caf-4009-8d9e-91f67c5aa7d0";
     private static final String BASE_URL = "http://datapoint.metoffice.gov.uk/public/data/";
+    private static final String LOCATION_LIST = "val/wxobs/all/json/sitelist";
+
 }
