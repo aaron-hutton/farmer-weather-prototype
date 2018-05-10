3db1c0627f9832fdc38911e228bf74f152a4c1ca
Roddy <rlm72@cam.ac.uk>
2018-05-10 15:11:36 +0100

    Added group19.iml

--- src/uk/ac/cam/cl/interaction_design/group19/app/MetOfficeAPI.java
+++ src/uk/ac/cam/cl/interaction_design/group19/app/MetOfficeAPI.java
@@ -2,4 +2,5 @@ package uk.ac.cam.cl.interaction_design.group19.app;
 
 public class MetOfficeAPI {
     private static final String KEY = "6d150446-6caf-4009-8d9e-91f67c5aa7d0";
+    private static final String BASE_URL = "http://datapoint.metoffice.gov.uk/public/data/";
 }
