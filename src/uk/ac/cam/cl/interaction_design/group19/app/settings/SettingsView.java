a98f5e5a4da55b44347a98a37eff1b68e9c85cfc
AARON-ASUS\Aaron <aaron@the-huttons.co.uk>
2018-05-10 16:07:31 +0100

    Add tabs at bottom, add view files

---
+++ src/uk/ac/cam/cl/interaction_design/group19/app/settings/SettingsView.java
@@ -0,0 +1,11 @@
+package uk.ac.cam.cl.interaction_design.group19.app.settings;
+
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class SettingsView extends JPanel {
+    public SettingsView() {
+        JLabel settings = new JLabel("Settings");
+        this.add(settings);
+    }
+}
