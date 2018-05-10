6c5c6dac6a31ce20c15fa879efded7967c93557c
nikihowe <nkhrrghw@gmail.com>
2018-05-10 15:28:23 +0100

    Try to commit z

--- src/uk/ac/cam/cl/interaction_design/group19/app/HelloWorld.java
+++ src/uk/ac/cam/cl/interaction_design/group19/app/HelloWorld.java
@@ -2,7 +2,7 @@ package uk.ac.cam.cl.interaction_design.group19.app;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        var hello = "Hello Worlds";
+        var hello = "Hello Worldz";
         System.out.println(hello);
     }
 }
