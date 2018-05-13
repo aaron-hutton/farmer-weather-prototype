216a9f9851dd13b5dec4bd1d666d5dbe2356eeba
nikihowe <nkhrrghw@gmail.com>
2018-05-13 19:58:55 +0100

    Add satellite and rain map functionality

--- src/uk/ac/cam/cl/interaction_design/group19/app/MetOfficeAPI.java
+++ src/uk/ac/cam/cl/interaction_design/group19/app/MetOfficeAPI.java
@@ -4,8 +4,8 @@ package uk.ac.cam.cl.interaction_design.group19.app;
  * A static helper class to manage the Met Office API
  */
 public class MetOfficeAPI {
-    private static final String KEY = "6d150446-6caf-4009-8d9e-91f67c5aa7d0";
-    private static final String BASE_URL = "http://datapoint.metoffice.gov.uk/public/data/";
-    private static final String LOCATION_LIST = "val/wxobs/all/json/sitelist";
-
+    public static final String KEY = "6d150446-6caf-4009-8d9e-91f67c5aa7d0";
+    public static final String BASE_URL = "http://datapoint.metoffice.gov.uk/public/data/";
+    public static final String LOCATION_LIST = "val/wxobs/all/json/sitelist";
+    public static final String IMAGE_PATH = "layer/wxobs/all/json/capabilities";
 }
