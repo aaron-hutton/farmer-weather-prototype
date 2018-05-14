b1a1049117497196d2c25e904aac60722fb98e9d
Charlie <cl685@cam.ac.uk>
2018-05-14 22:28:29 +0100

    bizeeAPI mostly finished, exception catching todo

--- src/uk/ac/cam/cl/interaction_design/group19/app/GDDs/bizeeAPI.java
+++ src/uk/ac/cam/cl/interaction_design/group19/app/GDDs/bizeeAPI.java
@@ -1,6 +1,8 @@
 package uk.ac.cam.cl.interaction_design.group19.app.GDDs;
 
+import java.util.Calendar;
 import java.util.Date;
+import java.util.spi.CalendarDataProvider;
 import net.degreedays.api.AccountKey;
 import net.degreedays.api.DegreeDaysApi;
 import net.degreedays.api.SecurityKey;
@@ -16,18 +18,26 @@ import net.degreedays.api.data.LocationDataRequest;
 import net.degreedays.api.data.LocationDataResponse;
 import net.degreedays.api.data.Period;
 import net.degreedays.api.data.Temperature;
+import net.degreedays.time.Day;
+import net.degreedays.time.DayRange;
 
 public class bizeeAPI {
 
-    public double gddSince(int postCode, double baseTemp, Date start) {
+    public double gddSince(int postCode, double baseTemp, Date s) throws IllegalArgumentException {
 
         DegreeDaysApi api = new DegreeDaysApi(
                 new AccountKey("test-test-test"),
                 new SecurityKey("test-test-test-test-test-test-test-test-test-test-test-test-test"));
 
+        Calendar cal = Calendar.getInstance();
+        Day end = new Day(cal.get(Calendar.YEAR), cal.get(Calendar.MONTH) + 1, cal.get(Calendar.DAY_OF_MONTH));
+        cal.setTime(s);
+        Day start = new Day(cal.get(Calendar.YEAR), cal.get(Calendar.MONTH) + 1, cal.get(Calendar.DAY_OF_MONTH));
+        DayRange range = new DayRange(start, end);
+
         DatedDataSpec hddSpec = DataSpec.dated(
                 Calculation.heatingDegreeDays(Temperature.celsius(10)),
-                DatedBreakdown.monthly(Period.latestValues(12)));
+                DatedBreakdown.monthly(Period.dayRange(range)));
 
         LocationDataRequest request = new LocationDataRequest(
                 Location.postalCode("10036", "US"),
