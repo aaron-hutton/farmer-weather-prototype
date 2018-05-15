9ca30c9235c468b717f3b175410366e2b59e85c5
Roddy <rlm72@cam.ac.uk>
2018-05-15 15:34:47 +0100

    Added API method to get hourly data for a location

--- src/uk/ac/cam/cl/interaction_design/group19/app/WeatherData.java
+++ src/uk/ac/cam/cl/interaction_design/group19/app/WeatherData.java
@@ -5,14 +5,12 @@ public class WeatherData {
     public final double wind_speed;    // mph
     public final String wind_direction;
     public final String weather_type;
-    public final int precipitation_prob;
 
-    public WeatherData(double temperature, double wind_speed, String wind_direction, String weather_type, int precipitation_prob) {
+    public WeatherData(double temperature, double wind_speed, String wind_direction, String weather_type) {
         this.temperature = temperature;
         this.wind_speed = wind_speed;
         this.wind_direction = wind_direction;
         this.weather_type = weather_type;
-        this.precipitation_prob = precipitation_prob;
     }
 
 }
