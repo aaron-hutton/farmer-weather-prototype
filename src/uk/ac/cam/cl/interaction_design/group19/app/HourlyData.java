96bb2416fe50cda128895d4d67f12fdf3170a1b6
Roddy <rlm72@cam.ac.uk>
2018-05-15 14:44:39 +0100

    Added class to represent hourly data

---
+++ src/uk/ac/cam/cl/interaction_design/group19/app/HourlyData.java
@@ -0,0 +1,9 @@
+package uk.ac.cam.cl.interaction_design.group19.app;
+
+public class HourlyData {
+    public double temperature;
+    public double wind_speed;
+    public String wind_direction;
+    public String weather_type;
+    public int precipitation_prob;
+}
--- src/uk/ac/cam/cl/interaction_design/group19/app/MetOfficeAPI.java
+++ src/uk/ac/cam/cl/interaction_design/group19/app/MetOfficeAPI.java
@@ -19,6 +19,8 @@ public class MetOfficeAPI {
     public static final String KEY = "6d150446-6caf-4009-8d9e-91f67c5aa7d0";
     public static final String BASE_URL = "http://datapoint.metoffice.gov.uk/public/data/";
     public static final String LOCATION_LIST = "val/wxobs/all/json/sitelist";
+    public static final String HOURLY_DATA = "val/wxobs/all/json/"; // + location_id
+    public static final String HOURLY_LOCATION_LIST = "val/wxobs/all/json/sitelist";
     public static final String IMAGE_PATH = "layer/wxobs/all/json/capabilities";
 
     private String addParam(String base_url, String key, Object value) {
@@ -58,15 +60,39 @@ public class MetOfficeAPI {
         return root.getAsJsonObject();
     }
 
-    public List<String> locationList() {
-        List<String> result = null;
+    public URL makeURL(String resource_part) {
         URL url = null;
         try {
-            url = new URL(addParam(BASE_URL + LOCATION_LIST, "key", KEY));
+            url = new URL(addParam(BASE_URL + resource_part, "key", KEY));
         } catch (MalformedURLException e) {
             System.err.println("Malformed URL error");
-            return result;
+            return null;
         }
+        return url;
+    }
+
+    public List<String> locationList() {
+        List<String> result = null;
+        URL url = makeURL(LOCATION_LIST);
+        if (url == null) return result;
+        JsonObject obj = jsonFromUrl(url);
+        System.out.println(obj.toString());
+        return result;
+    }
+
+    public List<String> hourlyLocationList() {
+        List<String> result = null;
+        URL url = makeURL(HOURLY_LOCATION_LIST);
+        if (url == null) return result;
+        JsonObject obj = jsonFromUrl(url);
+        System.out.println(obj.toString());
+        return result;
+    }
+
+    public Object hourlyData(int location_id) {
+        Object result = null;
+        URL url = makeURL(addParam(HOURLY_DATA + Integer.toString(location_id), "res", "hourly"));
+        if (url == null) return result;
         JsonObject obj = jsonFromUrl(url);
         System.out.println(obj.toString());
         return result;
@@ -74,6 +100,7 @@ public class MetOfficeAPI {
 
     public static void main(String[] args) {
         MetOfficeAPI api = new MetOfficeAPI();
-        api.locationList();
+        api.hourlyLocationList();
+        api.hourlyData(3066);
     }
 }
