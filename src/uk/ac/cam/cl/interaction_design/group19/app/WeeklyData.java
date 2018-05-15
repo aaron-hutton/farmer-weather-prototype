73edb9f33d76a57bb296f1a8300cbf77bca6a8de
Roddy <rlm72@cam.ac.uk>
2018-05-15 15:19:07 +0100

    Added constructor to weekly data

--- src/uk/ac/cam/cl/interaction_design/group19/app/WeeklyData.java
+++ src/uk/ac/cam/cl/interaction_design/group19/app/WeeklyData.java
@@ -1,4 +1,7 @@
 package uk.ac.cam.cl.interaction_design.group19.app;
 
 public class WeeklyData extends WeatherData {
+    public WeeklyData(double temperature, double wind_speed, String wind_direction, String weather_type, int precipitation_prob) {
+        super(temperature, wind_speed, wind_direction, weather_type, precipitation_prob);
+    }
 }
